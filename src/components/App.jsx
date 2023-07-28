eac3c278074bfaddd36e36cf65de9c6573f8b0b9
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-28 12:42:22 +0200

    routing to components?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
-import ContactForm from './ContactForm/ContactForm';
-import ContactList from './ContactList/ContactList';
-import Filter from './Filter/Filter';
-import css from './app.module.css'
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Home = React.lazy(() => import('./components/Home/Home'));
+const Movies = React.lazy(() => import('./components/Movies/Movies'));
+const MovieDetails = React.lazy(() => import('./components/MovieDetails/MovieDetails'));
+const Cast = React.lazy(() => import('./components/Cast/Cast'));
+const Reviews = React.lazy(() => import('./components/Reviews/Reviews'));
 
 const App = () => {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const storedContacts = localStorage.getItem('contacts');
-    if (storedContacts) {
-      setContacts(JSON.parse(storedContacts));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const handleAddContact = (name, number) => {
-    const contact = {
-      id: nanoid(),
-      name,
-      number
-    };
-
-    const isContactExists = contacts.some(
-      (contact) => contact.name.toLowerCase() === name.toLowerCase()
-    );
-
-    if (isContactExists) {
-      alert(`${name} is already in contacts.`);
-      return;
-    }
-
-    setContacts((prevContacts) => [...prevContacts, contact]);
-  };
-
-  const handleDeleteContact = (contactId) => {
-    setContacts((prevContacts) =>
-      prevContacts.filter((contact) => contact.id !== contactId)
-    );
-  };
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
   return (
-    <div>
-      <h1 className={css.h1}>Phonebook</h1>
-      <ContactForm onAddContact={handleAddContact} />
-      <h2>Contacts</h2>
-      <Filter filter={filter} onFilterChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies" component={Movies} />
+          <Route exact path="/movies/:movieId" component={MovieDetails} />
+          <Route path="/movies/:movieId/cast" component={Cast} />
+          <Route path="/movies/:movieId/reviews" component={Reviews} />
+          {/* Dodaj trasę dla nieistniejących stron */}
+          <Route component={Home} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 };
 
