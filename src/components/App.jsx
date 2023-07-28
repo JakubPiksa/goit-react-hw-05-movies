adebcacc2b01bc4a794b6c5fa0d0172321c4f380
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-28 13:34:04 +0200

    hmm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-
 const Home = React.lazy(() => import('./Home/Home'));
 const Movies = React.lazy(() => import('./Movies/Movies'));
 const MovieDetails = React.lazy(() => import('./MovieDetails/MovieDetails'));
@@ -12,15 +11,11 @@ const App = () => {
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
-        
-          <Route exact path="/" component={Home} />
-          <Route exact path="/movies" component={Movies} />
-          <Route exact path="/movies/:movieId" component={MovieDetails} />
-          <Route path="/movies/:movieId/cast" component={Cast} />
-          <Route path="/movies/:movieId/reviews" component={Reviews} />
-          {/* Dodaj trasę dla nieistniejących stron */}
-          <Route component={Home} />
-        
+        <Route exact path="/" component={Home} />
+        <Route exact path="/movies" component={Movies} />
+        <Route exact path="/movies/:movieId" component={MovieDetails} />
+        <Route path="/movies/:movieId/cast" component={Cast} />
+        <Route path="/movies/:movieId/reviews" component={Reviews} />
       </Suspense>
     </Router>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,41 @@
-const Home = () => {
-    return (
-        <p>I love react</p>
-    )
-}
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default Home
+const Home = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchTrendingMovies = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.themoviedb.org/3/trending/movie/day',
+          {
+            params: {
+              api_key: '610d75fe699fa5ab11363f277512029e',
+            },
+          }
+        );
+
+        setTrendingMovies(response.data.results);
+      } catch (error) {
+        console.error('Błąd podczas pobierania:', error);
+      }
+    };
+
+    fetchTrendingMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Najpopularniejsze filmy</h1>
+      <ul>
+        {trendingMovies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+      <p>Lubie filmy</p>
+    </div>
+  );
+};
+
+export default Home;
