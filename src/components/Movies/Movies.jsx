5bf38e625a91a716d8b2abd95ad91440f1536a29
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-28 19:01:08 +0200

    MOvies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,7 +1,59 @@
-const Movies = () => {
-    return (
-        <p>I love react</p>
-    )
-}
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_KEY } from 'constData/PermaData';
 
-export default Movies 
+
+const Movies = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    if (searchQuery) {
+      fetchMovies();
+    } else {
+      setSearchResults([]);
+    }
+  }, [searchQuery]);
+
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get(
+        'https://api.themoviedb.org/3/search/movie',
+        {
+          params: {
+            api_key: '610d75fe699fa5ab11363f277512029e',
+            query: searchQuery,
+          },
+        }
+      );
+
+      setSearchResults(response.data.results);
+    } catch (error) {
+      console.error('Error while fetching movies:', error);
+    }
+  };
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Movie Search</h1>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={handleSearch}
+        placeholder="Enter movie title..."
+      />
+      <ul>
+        {searchResults.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Movies;
+
--- src/constData/PermaData.js
+++ src/constData/PermaData.js
@@ -1,2 +1,2 @@
-export const API_KEY = '610d75fe699fa5ab11363f277512029e'
-export const API_URL = 'https://api.themoviedb.org/3'
+// export const API_KEY = '610d75fe699fa5ab11363f277512029e'
+// export const API_URL = 'https://api.themoviedb.org/3'
