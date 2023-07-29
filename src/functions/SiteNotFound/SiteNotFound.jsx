d2d3080330970db9ae547590d580d58240b0344e
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-29 20:11:21 +0200

    not found

--- src/functions/SiteNotFound/SiteNotFound.jsx
+++ src/functions/SiteNotFound/SiteNotFound.jsx
@@ -8,9 +8,9 @@ const SiteNotFound = () => {
       <h3 className={css.error} >
         I apologize for any confusion. It appears that I may have lost my way in the vast expanse of the internet. Allow me to return you to the world of movies, where we can continue our journey together. Please click the button below to return to the planet of movies.
       </h3>
-      <Link to="/">
-        <button>Go back</button>
-      </Link>
+      <button className={css.goBack_button}>
+        <Link to={`/`} >Go Back</Link>
+      </button>
     </div>
   );
 };
